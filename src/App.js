6b7ff150e11a5f96b74f52be861fc59c779ffbae
ritismia <157569944+ritismia@users.noreply.github.com>
2024-11-21 19:43:38 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import Home from './Components/Home'
+import Contents from "./Components/Contents"
+import Testimonial from './Components/Testimonial';
+import Features from './Components/Features';
+import FAQ from './Components/FAQ';
+import Blog from './Components/Blog';
+import Footer from './Components/Footer';
+
+
+function App() {
+  return (
+    <div>
+<Home/>
+<Contents/>
+<Testimonial/>
+<Features/>
+<FAQ/>
+ <Blog/> 
+<Footer/>
+    </div>
+  );
+}
+
+export default App;
